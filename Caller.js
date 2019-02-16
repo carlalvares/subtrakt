a1cad0e4204f00b45c1b0d031ffa7115ecc53a5f
Carl Alvares <alvares.carl@gmail.com>
2019-02-16 11:54:42 -0500

    Delete Caller.js

--- Caller.js
+++
@@ -1,6 +0,0 @@
-chrome.webNavigation.onCompleted.addListener(function() {
-    alert("spotify subscription");
-}, {url: [{urlMatches : 'https://www.spotify.com/us/account/subscription/*'}]});
-chrome.webNavigation.onCompleted.addListener(function() {
-    alert("netflix subscription");
-}, {url: [{urlMatches : 'https://www.netflix.com/BillingActivity'}]});
