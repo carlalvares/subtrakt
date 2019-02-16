c01ae51e145dea4640a4813ab4c9b4ef6c21646c
ttc290 <ttc290@nyu.edu>
2019-02-16 16:37:22 +0000

    using regex and match pattern

--- background.js
+++ background.js
@@ -4,7 +4,8 @@ chrome.webNavigation.onCompleted.addListener(function() {
 }, {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
 
 chrome.runtime.onMessage.addListener(function (message) {
-  const x = message.search("subscription-status");
-  const y = message.substring(x, x+150);
-  alert(y);
+  const pattern = 'subscription-status[^/]*</p>';
+  const x = message.match(pattern);
+  //const y = message.substring(x, x + 150);
+  alert(x);
 });
