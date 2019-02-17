f8430e5112a46163154f6f6576f6e3c65be5b5c1
Carl Alvares <alvares.carl@gmail.com>
2019-02-17 11:50:14 -0500

    modified alert to update billing date, merged all other branches

--- background.js
+++ background.js
@@ -4,22 +4,16 @@ chrome.webNavigation.onCompleted.addListener(function() {
 }, {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
 
 chrome.runtime.onMessage.addListener(function (message) {
-
-  //const pattern = 'subscription-status[^\.]*\.';
-  //const y = message.match(pattern);
   const x = message.search("recurring-date");
   if( x == -1) {
-  	localStorage["spotifydat"] = "free";
+  	localStorage["spotifydat"] = null;
   } else {
-	  //const sub = message.s
 	  const sub = message.substring(x+16, x + 30);
 	  const num = sub.search("</b>");
 	  const result = sub.substring(0,num);
-	  localStorage["spotifydat"] = result;
-	  //alert(localStorage["status"]);
+	  localStorage["spotifydat"] = new Date(result);
 	}
 
-	//for price
 	const temp = message.search("recurring-price");
   if(temp == -1) {
   	localStorage["spotifyprc"] = "free";
@@ -28,9 +22,7 @@ chrome.runtime.onMessage.addListener(function (message) {
 	  const numi = subi.search("</b>");
 	  alert(numi);
 	  const resulti = subi.substring(0,numi);
-	  //alert(resulti);
 	  localStorage["spotifyprc"] = resulti;
-	  //alert(localStorage["status"]);
 }
 
 
--- billingReminder.js
+++ billingReminder.js
@@ -1,14 +1,26 @@
-setTimeout(function() {
+checkBillingDates();
+
+setTimeout(checkBillingDates, 86400000)
+
+function checkBillingDates() {
   const today = new Date();
 
-  const spotifyBillingDate = new Date("February 19, 2019");
+  const spotifyBillingDate = new Date(localStorage["spotifydat"]);
   const netflixBillingDate = new Date(localStorage["netflixdat"]);
 
-  if (today.getDate() == (spotifyBillingDate.getDate() - 1)) {
-    alert("Your Spotify bill is due tomorrow!");
+  if (spotifyBillingDate !== "null") {
+    if (today.getDate() == (spotifyBillingDate.getDate() - 1)) {
+      alert("Your Spotify bill is due tomorrow!");
+      const spotifyUpdatedDate = spotifyBillingDate + 30;
+      localStorage["spotifydat"] = spotifyUpdatedDate;
+    }
   }
 
-  if today.getDate() == (netflixBillingDate.getDate() - 1)) {
-    alert("Your Netflix bill is due tomorrow!");
+  if (netflixBillingDate !== "null") {
+    if (today.getDate() == (netflixBillingDate.getDate() - 1)) {
+      alert("Your Netflix bill is due tomorrow!");
+      const netflixUpdatedDate = netflixBillingDate + 30;
+      localStorage["netflixdat"] = netflixUpdatedDate;
+    }
   }
-}, 86400000)
+}
